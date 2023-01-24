2bf9a786cd99213dd28d1e4e98df449db9357218
Gözde G <gzde.gurler@gmail.com>
2023-01-24 18:19:06 +0300

    14 passed.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,107 @@
 // post routerları buraya yazın
+const express = require("express");
+
+const PostModel = require("./posts-model");
+
+const router = express.Router();
+
+router.use(express.json());
+
+router.get("/", (req, res) => {
+  PostModel.find()
+    .then((posts) => {
+      res.json(posts);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Gönderiler alınamadı" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  PostModel.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        res
+          .status(404)
+          .json({ message: "Belirtilen ID'li gönderi bulunamadı" });
+      }
+      res.status(200).json(user);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Gönderi bilgisi alınamadı" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const post = req.body;
+
+  if (!post.title || !post.contents) {
+    res
+      .status(400)
+      .json({ message: "Lütfen gönderi için bir title ve contents sağlayın" });
+  } else {
+    PostModel.insert(post)
+      .then((createdPost) => {
+        res.status(201).json(createdPost);
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ message: "Veritabanına kaydedilirken bir hata oluştu" });
+      });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const post = req.body;
+  try {
+    const relatedPost = await PostModel.findById(req.params.id);
+    if (!relatedPost) {
+      res.status(404).json({ message: "Belirtilen ID'li gönderi bulunamadı" });
+    } else {
+      if (!post.title || !post.contents) {
+        res
+          .status(400)
+          .json({ message: "Lütfen gönderi için title ve contents sağlayın" });
+      } else {
+        const updatedPost = await PostModel.update(req.params.id, post);
+        res.status(200).json(updatedPost);
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Gönderi bilgileri güncellenemedi" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const relatedPost = await PostModel.findById(req.params.id);
+    if (!relatedPost) {
+      res.status(404).json({ message: "Belirtilen ID li gönderi bulunamadı" });
+    } else {
+      const deletedPost = await PostModel.remove(req.params.id);
+      res.status(200).json(deletedPost);
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Gönderi silinemedi" });
+  }
+});
+
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const relatedPost = await PostModel.findCommentById(req.params.id);
+    if (!relatedPost) {
+      res.status(404).json({ message: "Girilen ID'li gönderi bulunamadı." });
+    } else {
+      await PostModel.findPostComments(req.params.id).then((post) => {
+        res.json(post);
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Yorumlar bilgisi getirilemedi" });
+  }
+});
+
+router.delete("/:id/comments", async (req, res) => {});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // require your server and launch it here
+const server = require("./api/server");
+
+const port = 9000;
+
+server.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
+
+// server.get("/", (req, res) => {
+//   res.send("Hello");
+// });
